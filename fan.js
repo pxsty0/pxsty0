84ec9ee76486889c921032f3693cd4491a365437
pxsty <75846829+pxsty0@users.noreply.github.com>
2022-11-27 12:27:01 +0300

    Create fan.js

---
+++ fan.js
@@ -0,0 +1,43 @@
+const { exec } = require("child_process");
+if(process.argv[0] == "on"){
+
+
+exec("raspi-gpio set 14 dh", (error, stdout, stderr) => {
+    if (error) {
+        console.log(error);
+        return;
+    }
+    if (stderr) {
+        console.log(stderr);
+        return;
+    }
+    console.log(stdout);
+});
+  
+}
+else if(process.argv[0] == "off"){
+exec("raspi-gpio set 14 dl", (error, stdout, stderr) => {
+    if (error) {
+        console.log(error);
+        return;
+    }
+    if (stderr) {
+        console.log(stderr);
+        return;
+    }
+    console.log(stdout);
+});
+}
+else if(process.argv[0] == "status"){
+exec("raspi-gpio get 14", (error, stdout, stderr) => {
+    if (error) {
+        console.log(error);
+        return;
+    }
+    if (stderr) {
+        console.log(stderr);
+        return;
+    }
+    console.log(stdout);
+});
+}

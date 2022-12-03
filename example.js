2921ef61141cd9ccf941a2d77a6a09ad4959385e
pxsty <75846829+pxsty0@users.noreply.github.com>
2022-12-03 22:56:20 +0300

    Update example.js

--- example.js
+++ example.js
@@ -1,1 +1,8 @@
-console.log("hello world pxserv")
+var http = require('http');
+
+//create a server object:
+http.createServer(function (req, res) {
+  console.log("request geldi")
+  res.write('Hello World!'); //write a response to the client
+  res.end(); //end the response
+}).listen(8080);

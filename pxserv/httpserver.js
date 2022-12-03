f29d7eaf5949dc0dc45e6c117c8c422daf684956
pxsty <75846829+pxsty0@users.noreply.github.com>
2022-12-03 23:01:07 +0300

    Update httpserver.js

--- pxserv/httpserver.js
+++ pxserv/httpserver.js
@@ -6,3 +6,16 @@ http.createServer(function (req, res) {
   res.write('Hello World!'); //write a response to the client
   res.end(); //end the response
 }).listen(8080);
+const localtunnel = require('localtunnel');
+
+(async () => {
+  const tunnel = await localtunnel({ port: 8080 });
+
+  // the assigned public url for your tunnel
+  // i.e. https://abcdefgjhij.localtunnel.me
+  console.log(tunnel.url)
+
+  tunnel.on('close', () => {
+    // tunnels are closed
+  });
+})();
